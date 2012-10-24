ba18b67d53dbf8e423e53adc902ef910f6bc34d0
TomtomLeDaronDesDarons <tretif@gmail.com>
2012-10-24 10:42:25 -0700

    ajout des fichiers liés aux promotions.

---
+++ ApplicationENI/Modele/Promotion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationENI.Modele
+{
+    class Promotion
+    {
+        public String _code { get; set; }
+        public String _libelle { get; set; }
+
+        public Promotion(String pCode, String pLibelle) {
+            this._code = pCode;
+            this._libelle = pLibelle;
+        }
+
+        public override string ToString()
+        {
+            return _code;
+        }
+    }
+
+}
