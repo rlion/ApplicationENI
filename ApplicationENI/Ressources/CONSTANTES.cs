18aaa5a50272205fa5a086e1b2957f43272418f3
TomtomLeDaronDesDarons <tretif@gmail.com>
2012-11-17 02:23:12 -0800

    Modification constantes, update du fichier de suivi de tests

--- ApplicationENI/Ressources/CONSTANTES.cs
+++ ApplicationENI/Ressources/CONSTANTES.cs
@@ -12,7 +12,7 @@ namespace ApplicationENI.Ressources
         public static int TYPE_FORMATION_TOUS = 0;
         public static int TYPE_FORMATION_FC = 1;
         public static int TYPE_FORMATION_CP = 2;
-        public static String PREFIXE_PHOTO = "c:/testPhotos/";
+        public static String PREFIXE_PHOTO = "c:/testPhotos/"; //TODO: en production ce sera "\\srv-appli-eni\Photomaton\"
 
     }
 }
